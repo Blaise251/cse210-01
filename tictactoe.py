49cc42d27f8abc9805c76553178481db8e68b01b
Mouzoun Akomabou Blaise <65378899+Blaise251@users.noreply.github.com>
2022-01-16 02:36:26 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -18,9 +18,9 @@ def create_board():
 def display_board(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-----')
+    print('-+-+-')
     print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-----')
+    print('-+-+-')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
     
